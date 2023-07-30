262b558e6998cdac94284d2acdb52f9bc0e694eb
Josh Ryland <jryland@protonmail.com>
2023-07-30 11:00:05 +0200

    las commit?

--- app/components/title/mainPageTitle.tsx
+++ app/components/title/mainPageTitle.tsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const MainPageTitle = () => {
   return (
-    <div className="flex items-center justify-center w-full h-screen md:h-[50rem] md:w-[70rem] bg-primary-500 drop-shadow-md rounded-sm">
-      <div className='flex flex-col justify-between h-full w-1/2'>
-        <div className='flex flex-col justify-start'>
-          <h1 className='text-[10rem] text-left mb-0 leading-none'>Joshua</h1>
-          <h1 className='text-[10rem] text-left mb-0 leading-none'>Ryland</h1>
+    <div className="flex h-screen w-full items-center justify-center rounded-sm bg-primary-500 drop-shadow-md md:h-[65vh] md:w-[70rem]">
+      <div className="flex h-full w-full flex-col justify-between">
+        <div className="flex flex-col justify-start">
+          <div className="flex flex-row justify-between">
+            <div>
+            <h6 className="text-left text-base font-bold leading-none">
+                Joshua
+              </h6>
+              <h6 className="text-left text-base font-bold leading-none">
+                Ryland
+              </h6>
+            </div>
+            <div>
+              <h6 className="text-right text-base font-bold leading-none">
+                My Timeline
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-center font-bold leading-none text-5xl">
+            Just another developer portfolio...
+          </h3>
+          <h3 className="text-center font-bold leading-none text-5xl">
+            and a blog
+          </h3>
+        </div>
+        <div className="flex flex-row justify-between">
+        <div className="p flex flex-col justify-start pb-2 text-white">
+          <h6 className="text-left text-base font-bold leading-none">
+            Paris, France
+          </h6>
+          <h6 className="mb-0 text-left text-base font-bold leading-none">
+            London, England
+          </h6>
+          <h6 className="mb-0 text-left text-base font-bold leading-none">
+            Remote, My Study
+          </h6>
+        </div>
+        <div className="p flex flex-col justify-start pb-2 text-white">
+          <h6 className="text-right text-base font-bold leading-none">
+            Frontend
+          </h6>
+          <h6 className="mb-0 text-right text-base font-bold leading-none">
+            Software Engineer
+          </h6>
+          <h6 className="mb-0 text-right text-base font-bold leading-none">
+            Backend
+          </h6>
+        </div>
         </div>
       </div>
-      <div className='flex flex-col justify-between h-full w-1/2'>
-
-      </div>
-    {/* <div className="Group1 w-[845px] h-[508px] relative">
-  <div className="Rectangle2 w-[740px] h-[416px] left-[105px] top-[1px] absolute bg-stone-400 rounded shadow" />
-  <div className="JustAnotherDeveloperPortfolioAndABlog w-[338px] h-[49px] left-[499px] top-[366px] absolute text-right"><span className="text-white text-[20px] font-bold">Just </span><span className="text-green-600 text-[20px] font-bold">a</span><span className="text-white text-[20px] font-bold">nother developer</span><span className="text-zinc-300 text-[20px] font-bold"> </span><span className="text-white text-[20px] font-bold">portfolio <br/>...and a </span><span className="text-violet-700 text-[20px] font-bold">blog</span></div>
-  <div className="ParisFranceLondonEnglandRemoteMyStudy w-[338px] h-[49px] left-[112px] top-[361px] absolute"><span className="text-white text-[12px] font-bold">Paris, France<br/>London, En</span><span className="text-green-600 text-[12px] font-bold">g</span><span className="text-white text-[12px] font-bold">land<br/></span><span className="text-green-600 text-[12px] font-bold">R</span><span className="text-white text-[12px] font-bold">emote, My Study<br/></span></div>
-  <div className="JoshuaRyland w-[436px] h-[231px] left-[112px] top-0 absolute"><span className="text-zinc-300 text-[96px] font-bold">Joshua<br/></span><span className="text-violet-700 text-[96px] font-bold">Ryl</span><span className="text-green-600 text-[96px] font-bold">an</span><span className="text-violet-700 text-[96px] font-bold">d</span></div>
-  <div className="FrontendSoftwareEngineerBackend left-[706px] top-[14px] absolute text-right"><span className="text-zinc-300 text-[14px] font-bold">Frontend<br/>Softw</span><span className="text-green-600 text-[14px] font-bold">a</span><span className="text-zinc-300 text-[14px] font-bold">re Engineer<br/>Backend</span></div>
-  <div className="SoOriginal left-[785px] top-[351px] absolute text-right text-white text-[10px] font-thin">So original</div>
-  <div className="MytimeLine w-[89px] h-[33px] left-[715px] top-[188px] absolute"><span className="text-white text-[20px] font-thin">my</span><span className="text-white text-[20px] font-bold">ti</span><span className="text-green-600 text-[20px] font-bold">m</span><span className="text-white text-[20px] font-bold">e<br/>line</span></div>
-  <div className="Ellipse1 w-8 h-[30px] left-[793px] top-[193px] absolute rounded-full border border-zinc-300" />
-  <div className="WhyAreYouLookingHereActionsUpAbove left-0 top-[495px] absolute text-white text-[11px] font-thin">Why are you looking here? Actions up above.</div>
-</div> */}
     </div>
   );
 };
